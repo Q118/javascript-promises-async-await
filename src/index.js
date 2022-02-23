1d2694a9db56959112b41aa01ae7b1e2e689c360
Shelby Rothman <srothman@Sroth-7PZB163.corelation.local>
2022-02-23 07:37:13 -0800

    module 3 completed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-export function fetchMovies() {};
+const movies = require('./data/movies.json');
+import { fetchWithTimeout } from './services';
 
+export function fetchMovies() {
+    const resolveFunction = () => movies; //  returns the movies array
+    return fetchWithTimeout(1000).then(resolveFunction);
+};
 
+const moviesPromise = fetchMovies();
+
+moviesPromise.then(results => {
+    console.log(results);
+});
 
