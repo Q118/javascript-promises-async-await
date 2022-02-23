c8009c9e75a4625ffa0ba35b16afc90d4c13fd2a
Shelby Rothman <srothman@Sroth-7PZB163.corelation.local>
2022-02-23 08:03:43 -0800

    module 4

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import { fetchWithTimeout } from './services';
-const movies = require('./data/movies.json');
-export function fetchMovies() {
-    const resolveFunction = () => movies; //  returns the movies array
-    return fetchWithTimeout(1000).then(resolveFunction);
+import { fetchMovies, fetchBooks } from './services';
+
+
+// const movies = require('./data/movies.json');
+
+function getBooksAndMovies() {
+    return Promise.all([fetchBooks(), fetchMovies()])
+        .then(([books, movies]) => ({
+            books,
+            movies
+        }))
+        .catch(error => console.log("Error fetching books and movies", error));
+}
+
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then((results) => {
+    console.log("getBooksAndMoviesPromise", results);
+});
+
+function getBooksOrMovies() {
+    return Promise.race([fetchBooks(), fetchMovies()])
+        .then(results => results)
+        .catch(error => console.log("Error waiting for the promise race", error));
 };
 
-const moviePromise = fetchMovies();
-moviePromise.then(results => {
-    console.log(results);
+const getBooksOrMoviesPromise = getBooksOrMovies();
+getBooksOrMoviesPromise.then((results) => {
+    console.log('getBooksOrMoviesPromise', results)
 });
+// export function fetchMovies() {
+//     const resolveFunction = () => movies; //  returns the movies array
+//     return fetchWithTimeout(1000).then(resolveFunction);
+// };
+
+// const moviePromise = fetchMovies();
+// moviePromise.then(results => {
+//     console.log(results);
+// });
 
--- src/services.js
+++ src/services.js
@@ -1,5 +1,16 @@
-export function  fetchWithTimeout(delay) {
+export function fetchWithTimeout(delay) {
     return new Promise(resolve => setTimeout(resolve, delay));
 }
+export function fetchMovies() {
+    return fetch("./data/movies.json")
+        .then(response => response.json())
+        .then(movies => movies)
+        .catch(error => console.log(error));
+}
 
-module.exports = fetchWithTimeout;
+export function fetchBooks() {
+    return fetch("./data/books.json")
+        .then(response => response.json())
+        .then(books => books)
+        .catch(error => console.log(error));
+}
